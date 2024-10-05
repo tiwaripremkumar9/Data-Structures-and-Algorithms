78c48fd1a6dccc3a84853479271fe4557f1a90d9
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-10-05 19:49:09 +0530

    Refactoring Code

--- Sliding-Window/SlidingWindow.java
+++ Sliding-Window/SlidingWindow.java
@@ -12,31 +12,27 @@
      * Below Approach is code of my first try of sliding Algo
      */
     public static int slidingWindow(int [] arr, int k) {
-		if(k > arr.length) {
-			return -1;
-		}
-		int sum = 0;
-		for(int i = 0; i < k; i++) {
-			sum += arr[i];
-		}
-		
-		int maxSum = sum;
-		if(k == arr.length) {
-			return sum;
-		}
-		
-		for(int i = 1; i < arr.length; i++) {
-			int prevElement = arr[i - 1];
-			if(i + (k - 1) >= arr.length) {
-				break;
-			}
-			
-			int nextSumElement = arr[i + (k - 1)];
-			sum = (sum - prevElement) + nextSumElement;
-			maxSum = Math.max(maxSum, sum);
-		}
-		
-		return maxSum;
+        if(k > arr.length) {
+            return -1;
+        }
+        int sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        
+        int maxSum = sum;
+        if(k == arr.length) {
+            return sum;
+        }
+        
+        for(int i = k; i < arr.length; i++) {
+            //sum = sum + arr[i] - arr[i - k];
+            //rewriting above as shorthand
+            sum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum;
 	}
 
 
